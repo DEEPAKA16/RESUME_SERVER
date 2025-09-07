7c4f93d3361f4a51f1a48881c17ff2674c4c0348
DEEPAKA16 <deepakvika1610@gmail.com>
2025-09-07 22:43:47 +0530

    added cors

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
     })
 	r.Static("/uploads", "./uploads")
 	corsConfig := cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"https://student-smart-hub.web.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
